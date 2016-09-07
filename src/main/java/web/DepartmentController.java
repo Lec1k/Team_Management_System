2d34f3dad2313948ee1fac2c606589a8d75d51af
lec1k <lec1k2103@gmail.com>
2016-09-07 17:38:49 +0300

    Controllers configured. TO DO: views

--- src/main/java/web/DepartmentController.java
+++ src/main/java/web/DepartmentController.java
@@ -1,7 +1,51 @@
 package web;
 
+import domain.Department;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import service.DepartmentService;
+
+import java.util.Map;
+
 /**
  * Created by alvo0416 on 8/23/2016.
  */
+
+@Controller
 public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @RequestMapping("/deps")
+    public String listDepartments(Map<String,Object> map){
+
+        map.put("department", new Department());
+        map.put("departmentList", departmentService.listDepartment());
+
+        return "department";
+
+    }
+
+    @RequestMapping(value = "/addDept", method = RequestMethod.POST)
+    public String addDepartment(@ModelAttribute("department") Department department){
+
+        departmentService.addDepartment(department);
+        return "redirect:/deps";
+
+    }
+
+    @RequestMapping("/deleteDept/{departmentId}")
+    public String deleteDepartment(@PathVariable("departmentId") Integer departmentId){
+
+        departmentService.removeDepartment(departmentId);
+        return "redirect:/deps";
+
+    }
+
+
 }
--- src/main/java/web/EmployeeController.java
+++ src/main/java/web/EmployeeController.java
@@ -1,7 +1,49 @@
 package web;
 
+import domain.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import service.EmployeeService;
+
+import java.util.Map;
+
 /**
  * Created by alvo0416 on 8/23/2016.
  */
+@Controller
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @RequestMapping("/emps")
+    public String listEmployees(Map<String ,Object> map){
+
+        map.put("employee", new Employee());
+        map.put("employeeList", employeeService.listEmployee());
+
+        return "employee";
+
+    }
+
+    @RequestMapping(value = "/addEmpl", method = RequestMethod.POST)
+    public String addEmployee(@ModelAttribute("employee") Employee employee){
+
+        employeeService.addEmployee(employee);
+        return "redirect:/emps";
+
+    }
+
+    @RequestMapping("/deleteEmpl/{employeeId}")
+    public String removeEmployee(@PathVariable("employeeId") Integer employeeId){
+
+        employeeService.removeEmployee(employeeId);
+        return "redirect:/emps";
+    }
+
+
 }
